fb4cb6dc011bc5d4e5cfecc36b3284e8b7d5f683
Timothy John Manas <timothy.john.manas@gmail.com>
2016-05-18 18:25:26 -0400

    Initial version of ChatActivity

---
+++ app/src/main/java/x/y/z/ibmwatsonchatbot/activities/ChatActivity.java
@@ -0,0 +1,43 @@
+package x.y.z.ibmwatsonchatbot.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import x.y.z.ibmwatsonchatbot.R;
+
+/**
+ * Created by timmanas on 2016-05-18.
+ */
+public class ChatActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chatactivity);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+    }
+}
